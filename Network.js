faff4bc6d13324c2691b438393c9428e57886d3c
JSDevloop <56578188+JSDevloop@users.noreply.github.com>
2022-02-10 10:50:21 +0300

    u

--- Network.js
+++ Network.js
@@ -12,6 +12,18 @@ module.exports = class Network
         const express = require('express');
         const socketIo = require('socket.io');
         const app = express();
+        express.Router().get('/db', async (req, res) => {
+            try {
+              const client = await pool.connect();
+              const result = await client.query('SELECT * FROM test_table');
+              const results = { 'results': (result) ? result.rows : null};
+              res.render('pages/db', results );
+              client.release();
+            } catch (err) {
+              console.error(err);
+              res.send("Error " + err);
+            }
+          });
         this.server = http.createServer(app);
         this.io = socketIo(this.server);
         this.port = port;
