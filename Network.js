15d1707fb0baef4a23de2370039c698b732d6bf8
JSDevloop <56578188+JSDevloop@users.noreply.github.com>
2022-02-10 12:05:07 +0300

    Update Network.js

--- Network.js
+++ Network.js
@@ -30,7 +30,7 @@ module.exports = class Network
               const client = await pool.connect();
               const result = await client.query('SELECT * FROM test_table');
               const results = { 'results': (result) ? result.rows : null};
-              res.render('pages/db', results );
+              res.send( results );
               client.release();
             } catch (err) {
               console.error(err);
