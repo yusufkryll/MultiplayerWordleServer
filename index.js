05a715d5f24529b776667562977eda4fd048ff9f
JSDevloop <56578188+JSDevloop@users.noreply.github.com>
2022-02-10 20:31:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const Network = require('./Network');
 var port = process.env.PORT || 8080;
 var network = new Network(port, {});
 
-network.onConnect = (client, db) => {
+network.onConnect = async (client, db) => {
     console.log("A player connected: " + client.id);  
-    db.query('SELECT * FROM test_table', (err, result) => {
-        const results = { 'results': (result) ? result.rows : null};
-        console.log(results);
-        client.send("dbtest", "t");
-        db.release();
-    });
+    const result = await db.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    console.log(results);
+    client.emit("dbtest", results);
 };
