88da070a48fbe95a8b7cc0471533676a1a61a332
JSDevloop <56578188+JSDevloop@users.noreply.github.com>
2022-02-13 00:34:58 +0300

    Update Network.js

--- Network.js
+++ Network.js
@@ -12,6 +12,8 @@ module.exports = class Network
     }
     Start = (port) =>
     {
+        try
+        {
         const http = require('http');
         const express = require('express');
         const socketIo = require('socket.io');
@@ -29,12 +31,14 @@ module.exports = class Network
             res.send("<h1 style = 'color:red;'>No Page</h1>");
         })
         this.server = http.createServer(app);
-        this.io = socketIo(this.server);
+        this.io = socketIo(this.server, { 
+            transports: ['websocket'],
+            pingInterval: 1000 * 60 * 5,
+            pingTimeout: 1000 * 60 * 3
+        });
         this.port = port;
         this.Listen();
         this.io.on('connect', (client) => {
-            try
-            {
                 let getInPool = () => {
                     var poolRoom = this.io.sockets.adapter.rooms['pool'];
                     if(poolRoom != undefined) return poolRoom.sockets;
@@ -68,12 +72,12 @@ module.exports = class Network
                     }
                     console.log(getInPool());
                 })
-            }
-            catch (err)
-            {
-                console.log(err);
-            }
         });
+        }
+        catch (err)
+        {
+            console.log(err);
+        }
     }
     Listen = () =>
     {
@@ -81,4 +85,6 @@ module.exports = class Network
             console.log('listening on *:' + this.port);
         });
     }
+
+    
 }
