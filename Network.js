cc79d38705c027e9de192896848924d19905e096
JSDevloop <56578188+JSDevloop@users.noreply.github.com>
2022-02-13 11:43:35 +0300

    Update Network.js

--- Network.js
+++ Network.js
@@ -31,7 +31,9 @@ module.exports = class Network
             res.send("<h1 style = 'color:red;'>No Page</h1>");
         })
         this.server = http.createServer(app);
-        this.io = socketIo(this.server);
+        this.io = socketIo(this.server, {
+            upgradeTimeout: 30000 // default value is 10000ms, try changing it to 20k or more
+          });
         this.port = port;
         this.Listen();
         this.io.on('connect', (client) => {
