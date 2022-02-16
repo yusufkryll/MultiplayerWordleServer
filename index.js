b5cd5302c8b9a2ce77e0b363ab64fec8061ca4f6
JSDevloop <56578188+JSDevloop@users.noreply.github.com>
2022-02-16 14:48:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ var network = new Network(port, {});
 
 network.onConnect = async (client, db) => {
     console.log("A player connected: " + client.id);  
+    client.on("get-guest", async(data) => {
+        const result = await 
+        db.query(`SELECT * FROM users WHERE user_id = '${data}'`);
+        const results = { 'results': (result) ? result.rows : null};
+        client.log(results);
+    });
     client.on("guest-login", async (data) => {
         const result = await 
         db.query(`INSERT INTO users (user_id, user_name) VALUES ('${data.user_id}', '${data.user_name}')`);
