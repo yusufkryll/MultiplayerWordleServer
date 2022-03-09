e2a96e2415693ca48b06ad227a2f6e92de1104b4
JSDevloop <56578188+JSDevloop@users.noreply.github.com>
2022-03-09 22:50:47 +0300

    Update Network.js

--- Network.js
+++ Network.js
@@ -88,10 +88,8 @@ module.exports = class Network
 
                 client.on("Challenge", async (data) => {
                     let sockets = await this.io.fetchSockets();
-                    console.log(data);
                     var selectedSocket = sockets.find(s => s.data.public_id == data);
-                    console.log(selectedSocket);
-                    //selectedSocket.emit("Challenge", client.data.public_id);
+                    selectedSocket.emit("Challenge", client.data.public_id);
                 });
 
                 client.on("ChallengeAccept", async (data) => {
