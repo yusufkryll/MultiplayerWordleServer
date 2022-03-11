bcd590e6471488c57eccc0d33042f0f14224fb85
JSDevloop <56578188+JSDevloop@users.noreply.github.com>
2022-03-11 11:16:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,12 +24,12 @@ network.onConnect = (client, db) => {
     console.log("A player connected: " + client.id); 
     client.on("GetUserData", async() => {
         const result = await db.query(`SELECT * FROM users WHERE public_id = '${client.data.public_id}'`);
-        const result1 = result ? result.rows : null;
+        const result1 = result ? result.rows[0] : null;
         client.emit("GetUserData", result1);
     });
     client.on("GetArenas", async() => {
         const result = await db.query(`SELECT * FROM arenas`);
-        const result1 = result ? result.rows[0] : null;
+        const result1 = result ? result.rows : null;
         console.log(result1);
         client.emit("GetArenas", result1);
     });
